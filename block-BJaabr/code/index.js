ddd15bdfb5c70cc34d79df0aad402184f7797d3e
asvindra <aswin1399904@gmail.com>
2021-09-02 16:08:35 +0530

    Assignment: Function Types of Different types of functions

--- block-BJaabr/code/index.js
+++ block-BJaabr/code/index.js
@@ -41,18 +41,32 @@ Do the following with above problem.
 */
 
 // - Write a Function Declaration
+function addOne(number){
+  return number+1;
+}
 
 // - Write a Function Expression
+const addOne = function addOne(number){
+  return number+1;
+};
 
 // - Write an Arrow Function without curly brackets(if possible)
+const addOne = number=>number+1;
 
 // - Write an Arrow Function with curly brackets
+const addOne =number=>{
+  return number+1;
+
+};
 
 // - Execute the function
+addOne(4);
 
 // - Execute the function and store the return value in a variable.
+let returnValue=addOne(4);
 
 // - What is the typeof returnValue
+number
 
 /* 2. Subtract one
 
@@ -62,19 +76,25 @@ Do the following with above problem.
 */
 
 // - Write a Function Declaration
-
+function substractOne(number){
+  return number-1;
+}
 // - Write a Function Expression
-
+const substractOne = function substractOne(number){
+  return number-1;
+};
 // - Write an Arrow Function without curly brackets(if possible)
-
+const substractOne = number=>number-1;
 // - Write an Arrow Function with curly brackets
-
+const substractOne = number=>{
+  return number-1;
+};
 // - Execute the function
-
+substractOne(7);
 // - Execute the function and store the return value in a variable.
-
+let returnValue = substractOne(7);
 // - What is the typeof returnValue
-
+number
 /* 3. Add two numbers
 
 Write a function named `sum` that accepts two numbers and returns the sum of them.
@@ -83,19 +103,26 @@ Do the following with above problem.
 */
 
 // - Write a Function Declaration
-
+function sum(numA,numB){
+return numA+numB;
+}
 // - Write a Function Expression
+const sum = function sum(numA,numB){
+  return numA+numB;
+}
 
 // - Write an Arrow Function without curly brackets(if possible)
-
+const sum = (numA,numB)=>numA+numB;
 // - Write an Arrow Function with curly brackets
-
+const sum = (numA,numB)=>{
+  return numA+numB;
+}
 // - Execute the function
-
+sum(4,4);
 // - Execute the function and store the return value in a variable
-
+let returnValue = sum(4,4);
 // - What is the typeof returnValue
-
+number
 /* 4. Square of the given value
 
 Write a function named `square` that accepts a number and returns the square of the number.
@@ -104,19 +131,27 @@ Do the following with above problem.
 */
 
 // - Write a Function Declaration
-
+function square(number){
+  return number*number;
+}
 // - Write a Function Expression
+const square = function square(number){
+  return number*number;
+}
 
 // - Write an Arrow Function without curly brackets(if possible)
 
+const square = number=>number*number;
 // - Write an Arrow Function with curly brackets
-
+const square = number=>{
+  return number*number;
+}
 // - Execute the function
-
+square(7);
 // - Execute the function and store the return value in a variable
-
+let returnValue = square(7);
 // - What is the typeof returnValue
-
+number
 /* 5. Find isGreater
 
 Write a function named isGreater that accepts two numbers`x` and`y` and returns`true` if x is greater than y or return `false`.
@@ -125,19 +160,23 @@ Do the following with above problem.
 */
 
 // - Write a Function Declaration
-
+function isGreater(x,y){
+  return x>y?'true':'false';
+}
 // - Write a Function Expression
-
+const isGreater = function isGreater(x,y){
+  return  x>y?'true':'false';
+}
 // - Write an Arrow Function without curly brackets(if possible)
-
+const isGreater =(x,y)=>  x>y?'true':'false';
 // - Write an Arrow Function with curly brackets
-
+const isGreater =(x,y)=> {return x>y?'true':'false';}
 // - Execute the function
-
+isGreater(4,5);
 // - Execute the function and store the return value in a variable
-
+let returnValue = isGreater(4,5);
 // - What is the typeof returnValue
-
+String
 /* 6.
 
 Write a function named `oddOrEven` that accepts a`number` and returns
@@ -149,17 +188,26 @@ Do the following with above problem.
 */
 
 // - Write a Function Declaration
+function oddOrEven(number){
+  return number%2==0 ? `Number is even`: `Number is odd`;
+}
 
 // - Write an anonymous Function Expression
-
+const oddOrEven = function (number)
+{return number%2==0 ? `Number is even`: `Number is odd`;};
 // - Write an named Function Expression
-
+const oddOrEven = function oddOrEven(number){
+  return number%2==0 ? `Number is even`: `Number is odd`;
+};
 // - Write an Arrow Function without curly brackets (hint: use ternary operator)
-
+const oddOrEven = number=>number%2==0 ? `Number is even`: `Number is odd`;
 // - Write an Arrow Function with curly brackets
-
+const oddOrEven = number=>{
+  return number%2==0 ? `Number is even`: `Number is odd`;
+};
 // - Execute the function
-
+oddOrEven(7);
 // - Execute the function and store the return value in a variable
-
+let returnValue = oddOrEven(number);
 // - What is the typeof returnValue
+string
